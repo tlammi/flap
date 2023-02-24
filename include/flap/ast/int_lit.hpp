302789dc07e7c4e5bbcb7703121c1a3af88efe31
tlammi <tangojulietlima@gmail.com>
2023-02-24 21:10:05 +0200

    Value parsing

--- include/flap/ast/int_lit.hpp
+++ include/flap/ast/int_lit.hpp
@@ -4,14 +4,28 @@
 
 namespace flap::ast {
 
-struct IntLit {
+class IntLit {
+    static constexpr std::string_view hex = "0x";
+    static constexpr std::string_view bin = "0b";
+
+ public:
+    constexpr explicit IntLit(StringView val) : m_val{val} {}
     constexpr int radix() const noexcept {
-        if (value.starts_with("0x")) return 16;
-        if (value.starts_with("0b")) return 2;
+        if (m_val.starts_with(hex)) return 16;
+        if (m_val.starts_with(bin)) return 2;
         return 10;
     }
 
-    StringView value;
+    constexpr StringView value() const noexcept {
+        if (m_val.starts_with(hex) || m_val.starts_with(bin))
+            return m_val.substr(2);
+        return m_val;
+    }
+
+    constexpr StringView raw_value() const noexcept { return m_val; }
+
+ private:
+    StringView m_val;
 };
 
 }  // namespace flap::ast
--- lib/flap/test_ast.cpp
+++ lib/flap/test_ast.cpp
@@ -12,3 +12,14 @@ TEST(IntLit, Radix) {
     static constexpr IntLit b{"0b100"};
     ASSERT_EQ(b.radix(), 2);
 }
+
+TEST(IntLit, Value) {
+    constexpr IntLit d{"100"};
+    ASSERT_EQ(d.value(), "100");
+
+    constexpr IntLit h{"0x101"};
+    ASSERT_EQ(h.value(), "101");
+
+    constexpr IntLit b{"0b111"};
+    ASSERT_EQ(b.value(), "111");
+}
